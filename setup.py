0cba4414b9ad7374eab6c7f7c31d4893ac31d0a0
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-10-17 13:58:00 +0100

    version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dismal',
-    version='v0.1.3-alpha',
+    version='v0.1.4-alpha',
     packages=['dismal'],
     install_requires=[
         "numpy",
