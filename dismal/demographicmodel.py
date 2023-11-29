parameter scaling bugs fixed

dismal/demographicmodel.py
@@ -24,6 +24,10 @@ class DemographicModel:
         self.negll = None
         self.claic = None
         self.inferred_params = None
+        self.optim_object = None
+        self.stderr = None
+
+        self.demes_representation = None
 
 
     def add_epoch(self,
@@ -127,7 +131,9 @@ class DemographicModel:
         self.modelinstance = ModelInstance(optimised.x, self.epochs)
         self.negll = optimised.fun
         self.claic = self.cl_akaike(optimised)
+        self.stderr = self.standard_err(optimised)
         self.inferred_params = optimised.x
+        self.optim_object = optimised
 
         self.epochs = self.modelinstance.epochs
 
@@ -144,20 +150,34 @@ class DemographicModel:
             claic = None
         
         return claic
+    
+    def standard_err(self, optim_obj):
+        if hasattr(optim_obj, "hess_inv"):
+            stderr = np.sqrt(np.diag(optim_obj.hess_inv.todense()))
+        else:
+            stderr = None
+        
+        return stderr
 
 
-    def demes_format(self, mutation_rate):
+    def demes_format(self, mutation_rate, blocklen):
         """Represent model in Demes format"""
-        demes_rep = DemesRepresentation(self, mutation_rate=mutation_rate)
+        demes_rep = DemesRepresentation(self, mutation_rate=mutation_rate, blocklen=blocklen)
         self.demes_representation = demes_rep
         return demes_rep
 
 
-    def demesdraw(self, mutation_rate):
+    def demesdraw(self, mutation_rate=None, blocklen=None):
         """Draw Demes model; convenience function for demes_format().drawing"""
-        return self.demes_format(mutation_rate=mutation_rate).drawing
+        if self.demes_representation is None:
+            self.demes_representation = self.demes_format(mutation_rate=mutation_rate, blocklen=blocklen)
+
+        return self.demes_representation.drawing
 
 
-    def demesgraph(self, mutation_rate):
+    def demesgraph(self, mutation_rate=None, blocklen=None):
         """Return Demes graph; convenience function for demes_format().graph"""
-        return self.demes_format(mutation_rate=mutation_rate).graph
+        if self.demes_representation is None:
+            self.demes_representation = self.demes_format(mutation_rate=mutation_rate, blocklen=blocklen)
+
+        return self.demes_representation.graph

dismal/simulate.py
@@ -2,18 +2,19 @@ import msprime
 import numpy as np
 import random
 import itertools
+from collections import Counter
 
 
 class DemographySimulation:
 
-    def __init__(self, block_thetas, epoch_durations, migration_rates_fraction,
+    def __init__(self, block_thetas, epoch_durations, migration_rates,
                  blocklen=500, mutation_rate=1e-9, blocks_per_state=20_000, recombination_rate=0):
         """Msprime simulation
 
         Args:
             block_theta (iterable): Deme sizes in number of haploid individuals, in DISMaL order (backwards in time)
             epoch_durations (iterable): Durations of epoch 0 and 1 in 2Ne generations
-            migration_rates_fraction (iterable): Migration rates as fraction of population
+            migration_rates (iterable): Migration rates (big M/number of migrant genomes per generation
             blocklen (int): Length of simulated blocks
             mutation_rate (float): Mutation rate per base
             blocks_per_state (int, optional): Number of block simulations per simulation. Defaults to 20_000.
@@ -23,7 +24,6 @@ class DemographySimulation:
         # original parameters
         self.block_thetas = block_thetas
         self.epoch_durations = epoch_durations
-        self.migration_rates_fraction = migration_rates_fraction
         self.blocklen = blocklen
         self.mutation_rate = mutation_rate
         self.blocks_per_state = blocks_per_state
@@ -32,20 +32,22 @@ class DemographySimulation:
 
         # converted parameters for msprime
         self.site_theta = np.array(self.block_thetas)/self.blocklen
-        self.deme_sizes_2N = np.array(self.site_theta)/(2*self.mutation_rate)
-        self.epoch_durations_generations = np.array(self.epoch_durations) * self.deme_sizes_2N[2]
+        self.deme_sizes = np.array(self.site_theta)/(4*self.mutation_rate)
+        self.migration_rates_fraction = np.array(migration_rates)/(self.deme_sizes[0:4])
+        self.epoch_durations_generations = np.array(self.epoch_durations) * self.deme_sizes[2]
         self.split_times_generations = self.epoch_durations_generations[0], np.sum(self.epoch_durations_generations)
 
         self.demography = self.create_demography()
         self.tree_sequences = self.create_treesequences()
-        self.s1, self.s2, self.s3 = [self.add_mutations(ts) for ts in self.tree_sequences]
+        self.s1_counts, self.s2_counts, self.s3_counts = [self.add_mutations(ts) for ts in self.tree_sequences]
+        self.s1, self.s2, self.s3 = self.seg_sites_distr()
 
 
     def create_demography(self):
         demography = msprime.Demography()
 
         for deme_idx, deme_id in enumerate(self.deme_ids):
-            demography.add_population(name=deme_id, initial_size=self.deme_sizes_2N[deme_idx])
+            demography.add_population(name=deme_id, initial_size=self.deme_sizes[deme_idx])
 
         demography.add_population_split(self.split_times_generations[0], 
                                         derived=[self.deme_ids[0]], 
@@ -98,6 +100,21 @@ class DemographySimulation:
         s = np.array([ts_mut.divergence(sample_sets=[[0], [2]], span_normalise=False) for ts_mut in ts_muts])
             
         return s
+    
+    def seg_sites_distr(self):
+        seg_sites_spec = []
+
+        s1_counter, s2_counter, s3_counter = [Counter(counts) 
+                                              for counts in [self.s1_counts, self.s2_counts, self.s3_counts]]
+
+        for s_counter in [s1_counter, s2_counter, s3_counter]:
+            s_max = max(s_counter.keys()) + 1
+            s_arr = np.zeros(int(s_max))
+            s_arr[np.array(list(s_counter.keys())).astype(int)] = list(s_counter.values())
+            seg_sites_spec.append(s_arr)
+
+        return seg_sites_spec
+    
 
 
     @staticmethod    

tests/test_simulate.py
@@ -8,7 +8,7 @@ def setup_simulation():
     return DemographySimulation(
         block_thetas=[3,2,4,3,6],
         epoch_durations=[2,1],
-        migration_rates_fraction=[5e-8, 5e-8, 5e-9, 5e-9],
+        migration_rates=[0.3, 0.2, 0.2, 0.3],
         blocks_per_state = 100
     )
 
@@ -18,23 +18,17 @@ def test_site_theta(setup_simulation):
           [0.006, 0.004, 0.008, 0.006, 0.012]))
 
 
-def test_deme_sizes_2N(setup_simulation):
+def test_deme_sizes(setup_simulation):
     mutation_rate = 1e-9
-    assert_allclose(setup_simulation.deme_sizes_2N,
-                    np.array([0.006, 0.004, 0.008, 0.006, 0.012]) / (2 * mutation_rate)) 
+    assert_allclose(setup_simulation.deme_sizes,
+                    np.array([0.006, 0.004, 0.008, 0.006, 0.012]) / (4 * mutation_rate)) 
         
 
 def test_epoch_durations_generations(setup_simulation):
     assert_allclose(setup_simulation.epoch_durations_generations,
-                    np.array([8_000_000, 4_000_000]))
+                    np.array([4_000_000, 2_000_000]))
     
 
 def test_split_times_generations(setup_simulation):
     assert_allclose(setup_simulation.split_times_generations,
-                    np.array([8_000_000, 12_000_000]))
-    
-
-def test_sarrays_correct_length(setup_simulation):
-    n_blocks_per_state = 100
-    for s in [setup_simulation.s1, setup_simulation.s2, setup_simulation.s3]:
-        assert s.shape[0] == n_blocks_per_state 
+                    np.array([4_000_000, 6_000_000]))